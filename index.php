76e2f79bd51c491a9a975d29fd8080f913b3f00c
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-05 13:47:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
       <h1>MIS 4013 Final Project</h1>
-<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/de355201-269a-4f25-8aa4-3bd21e2a5117/df6eoa8-c88e8284-3f62-46dc-b090-69883eeb607e.png/v1/fit/w_828,h_496/music_note_symbol___outline_by_orxngecrxsh_df6eoa8-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjQ1NyIsInBhdGgiOiJcL2ZcL2RlMzU1MjAxLTI2OWEtNGYyNS04YWE0LTNiZDIxZTJhNTExN1wvZGY2ZW9hOC1jODhlODI4NC0zZjYyLTQ2ZGMtYjA5MC02OTg4M2VlYjYwN2UucG5nIiwid2lkdGgiOiI8PTQwOTYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.aaUv-5uBzZL54p0zvxi07UVyU0elMO4zqbww1wc2B9Q" style="width:50px;height:50px;">
 <?php
 include "view-footer.php";
 ?>
