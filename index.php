2a8b74d3636f5b5ab2c4480c5c061498419b52ae
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-05 13:41:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
       <h1>MIS 4013 Final Project</h1>
-<img src="https://visualpharm.com/assets/744/Musical%20Notes-595b40b85ba036ed117da66f.svg" style="width:50px;height:50px;">
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fvisualpharm.com%2Fassets%2F744%2FMusical%2520Notes-595b40b85ba036ed117da66f.svg&tbnid=aldgMa-1R-PjMM&vet=12ahUKEwic-IDmhfmCAxXl58kDHYasDpMQMyggegUIARDDAQ..i&imgrefurl=https%3A%2F%2Fwww.visualpharm.com%2Ffree-icons%2Fmusical%2520notes-595b40b85ba036ed117da66f&docid=pLVXfdjUbvZE6M&w=800&h=800&q=music%20note%20icon&ved=2ahUKEwic-IDmhfmCAxXl58kDHYasDpMQMyggegUIARDDAQ" style="width:50px;height:50px;">
 <?php
 include "view-footer.php";
 ?>
