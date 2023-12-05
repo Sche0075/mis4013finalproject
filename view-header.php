a8071b7f227fc09e75918575f16f0fcc2f32c286
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-05 15:03:45 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -10,16 +10,12 @@
     <div classs="container">
        <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
-
-            <div style="border-style:solid;border-color:black;border-width:1px;margin-right:10px;margin-left:10px;"">
              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-music-note-list" viewBox="0 0 16 16">
                 <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2"/>
                 <path fill-rule="evenodd" d="M12 3v10h-1V3z"/>
                 <path d="M11 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 16 2.22V4l-5 1z"/>
                 <path fill-rule="evenodd" d="M0 11.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5m0-4A.5.5 0 0 1 .5 7H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5m0-4A.5.5 0 0 1 .5 3H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5"/>
-              </svg>
-            </div>
-            
+              </svg>            
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
