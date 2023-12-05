0838660430acd0b9f92ca9a19846212cae5608d6
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-05 14:35:23 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,7 +9,7 @@
   <body style="margin-right:10px">
     <div classs="container">
        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
+          <div class="container-fluid" style="border-style:solid;border-color:black;">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-music-note-beamed" viewBox="0 0 16 16">
               <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13c0-1.104 1.12-2 2.5-2s2.5.896 2.5 2m9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2"/>
               <path fill-rule="evenodd" d="M14 11V2h1v9zM6 3v10H5V3z"/>
