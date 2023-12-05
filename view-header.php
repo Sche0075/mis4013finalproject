73d3ac366cb00130212c82835cbab7022083a61d
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-05 14:28:00 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -10,7 +10,11 @@
     <div classs="container">
        <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
-            <i class="bi bi-music-note-beamed"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-music-note-beamed" viewBox="0 0 16 16">
+              <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13c0-1.104 1.12-2 2.5-2s2.5.896 2.5 2m9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2"/>
+              <path fill-rule="evenodd" d="M14 11V2h1v9zM6 3v10H5V3z"/>
+              <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4z"/>
+            </svg>
             <a class="navbar-brand" href="#">Music Data Base</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
