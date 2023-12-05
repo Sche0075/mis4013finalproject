3ec008b077e9e808dc9d1b389ef5a2a66381e3da
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-05 12:57:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $pageTitle = "Home";
-include "header.php";
+include "view-header.php";
 ?>
       <h1>MIS 4013 Final Project</h1>
 <?php
-include "footer.php";
+include "view-footer.php";
 ?>
