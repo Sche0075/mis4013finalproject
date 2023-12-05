3de7d4b5830abef4ca1719a16cca5c22e8c19964
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-05 13:40:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
       <h1>MIS 4013 Final Project</h1>
+<img src="https://visualpharm.com/assets/744/Musical%20Notes-595b40b85ba036ed117da66f.svg" style="width:50px;height:50px;">
 <?php
 include "view-footer.php";
 ?>
