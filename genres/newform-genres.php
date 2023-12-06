c4aa8f1283c663fb521dc75724b6e8f46d836fe6
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-06 12:32:23 -0600

    Create newform-genres.php

---
+++ genres/newform-genres.php
@@ -0,0 +1,37 @@
+<!-- Button trigger modal -->
+<div style="height:16px;width:16px;margin-right:70px;margin-top:30px;">
+  <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#newGenreModal">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
+        <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2m5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672Z"/>
+        <path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5"/>
+      </svg>
+  </button>
+</div>
+  
+<!-- Modal -->
+<div class="modal fade" id="newGenreModal" tabindex="-1" aria-labelledby="newGenreModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+        <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="newGenreModalLabel">New Song</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+                  <div class="modal-body">
+                          <form method="post" action="">
+                                    <div class="mb-3">
+                                      <label for="gName" class="form-label">Genre Name</label>
+                                      <input type="text" class="form-control" id="gName" name="gName">
+                                    </div>
+                                        
+                                    <div class="mb-3">
+                                      <label for="gDescription" class="form-label">Genre Description</label>
+                                      <input type="text" class="form-control" id="gDescription" name="gDescription">
+                                    </div>                                  
+                                  
+                                <input type="hidden" name="actionType" value="Add">
+                              <button type="submit" class="btn btn-primary">Save</button>
+                          </form> 
+                  </div>      
+        </div> 
+  </div> 
+</div>
