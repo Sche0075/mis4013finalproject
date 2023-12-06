8321c0e220c19820d9bf9d24d3d238735d9ec5bb
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-06 03:48:16 -0600

    Update newform-records.php

--- records/newform-records.php
+++ records/newform-records.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<div style="height:16px;width:16px;margin-right:50px;margin-top:30px;">
+<div style="height:16px;width:16px;margin-right:70px;margin-top:30px;">
   <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#newRecordModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
         <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2m5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672Z"/>
