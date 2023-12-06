cccbc496094c01ac3a3e32d947018065003b5d78
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-06 12:18:05 -0600

    Update 3-genres.php

--- 3-genres.php
+++ 3-genres.php
@@ -1,1 +1,40 @@
+<?php
+require_once("util-db.php");
+require_once("genres/model-genres.php");
 
+$pageTitle = "Genres";
+include "headfoot/view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+     if (insertGenres($_POST['gName'], $_POST['gDescription'])) {
+       echo '<div class="alert alert-success" role="alert">Genre added.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+
+    case "Edit":
+     if (updateGenres($_POST['gName'], $_POST['gDescription'], $_POST['gid'])) {
+       echo '<div class="alert alert-success" role="alert">Genre edited.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+   
+    case "Delete":
+     if (deleteGenres($_POST['gid'])) {
+       echo '<div class="alert alert-success" role="alert">Genre deleted.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+    
+  }  
+}
+
+$songs = selectSongs();
+include "genres/view-genres.php";
+include "headfoot/view-footer.php";
+?>
