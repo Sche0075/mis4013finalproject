1a203c51c17327d527868fc63217e651c6d6b30e
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 13:37:55 -0600

    Update model-artists-by-records.php

--- artists-by-records/model-artists-by-records.php
+++ artists-by-records/model-artists-by-records.php
@@ -3,7 +3,7 @@
 function selectArtistByPlayer($abrid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.player_id, player_name, player_position, player_age, level, practice_time FROM `player` p join team t on t.player_id = p.player_id where t.player_id = ?");
+        $stmt = $conn->prepare("SELECT a.artist_id, a.artist_name, a.artist_hometown, r.record_name, g.genre_name FROM record r JOIN artist a ON a.record_id = r.record_id JOIN genre g ON g.genre_id = a.genre_id Where r.record_id = ?");
         $stmt->bind_param("i",$abrid);
         $stmt->execute();
         $result = $stmt->get_result();
