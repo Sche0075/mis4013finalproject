ef0683bdda81d3b828a7ebf1db8a228519c32637
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 14:19:40 -0600

    Create model-artists-by-genres.php

---
+++ artists-by-genres/model-artists-by-genres.php
@@ -0,0 +1,18 @@
+<?php
+
+function selectArtistsByRecords($abrid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT a.artist_id, a.artist_name, a.artist_hometown, r.record_name, g.genre_name FROM record r JOIN artist a ON a.record_id = r.record_id JOIN genre g ON g.genre_id = a.genre_id Where r.record_id = ?");
+        $stmt->bind_param("i",$abrid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
