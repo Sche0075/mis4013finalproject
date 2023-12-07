6cc714a1689797139d9455509b56acf503fb889f
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 13:07:06 -0600

    Update model-artists-by-records.php

--- artists-by-records/model-artists-by-records.php
+++ artists-by-records/model-artists-by-records.php
@@ -1,1 +1,18 @@
+<?php
 
+function selectArtistByPlayer($abrid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.player_id, player_name, player_position, player_age, level, practice_time FROM `player` p join team t on t.player_id = p.player_id where t.player_id = ?");
+        $stmt->bind_param("i",$abrid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
