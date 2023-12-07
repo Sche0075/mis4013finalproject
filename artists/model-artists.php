1a2fa229e2d28bcd07b0d1f57a49c9dd5bcb785a
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 15:33:40 -0600

    Create model-artists.php

---
+++ artists/model-artists.php
@@ -0,0 +1,59 @@
+<?php
+
+function selectArtists() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT artist_id, artist_name, artist_description FROM `artist`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertArtists($aName, $aHometown) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `artist` (`artist_name`, `artist_hometown`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $aName, $aHometown);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateArtists($aName, $aHometown, $aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `artist` set `artist_name` = ?, `artist_hometown` = ? where artist_id = ?");
+        $stmt->bind_param("ssi", $aName, $aHometown, $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from artist where artist_id = ?");
+        $stmt->bind_param("i", $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
