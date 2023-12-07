125278cadfbc56e5002f2ec53ed13577cffc0197
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 14:18:51 -0600

    Update 3-artists-by-genres.php

--- 3-artists-by-genres.php
+++ 3-artists-by-genres.php
@@ -1,1 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("artists-by-genres/model-artists-by-genres.php");
 
+$pageTitle = "Genre Artists";
+include "headfoot/view-header.php";
+
+$artistsbygenres = selectArtistsByGenres($_POST['abgid']);
+include "artists-by-genres/view-artists-by-genres.php";
+include "headfoot/view-footer.php";
+?>
