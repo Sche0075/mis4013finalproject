29ddbf343e89a0f1f239c6a2aeccf0ca4d4d10ec
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 13:03:48 -0600

    Update 5-artists-by-records.php

--- 5-artists-by-records.php
+++ 5-artists-by-records.php
@@ -1,40 +1,11 @@
 <?php
-require_once("util-db.php");
+require_once("b1-util-db.php");
 require_once("artist-by-records/model-artist-by-records.php");
 
-$pageTitle = "Artist";
+$pageTitle = "Record Label Artist";
 include "headfoot/view-header.php";
 
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-     if (insertGenres($_POST['gName'], $_POST['gDescription'])) {
-       echo '<div class="alert alert-success" role="alert">Genre added.</div>';
-     } else {
-       echo '<div class="alert alert-danger" role="alert">Error.</div>';
-     }
-        break;    
-
-    case "Edit":
-     if (updateGenres($_POST['gName'], $_POST['gDescription'], $_POST['gid'])) {
-       echo '<div class="alert alert-success" role="alert">Genre edited.</div>';
-     } else {
-       echo '<div class="alert alert-danger" role="alert">Error.</div>';
-     }
-        break;    
-   
-    case "Delete":
-     if (deleteGenres($_POST['gid'])) {
-       echo '<div class="alert alert-success" role="alert">Genre deleted.</div>';
-     } else {
-       echo '<div class="alert alert-danger" role="alert">Error.</div>';
-     }
-        break;    
-    
-  }  
-}
-
-$genres = selectGenres();
-include "genres/view-genres.php";
+$artistbyrecords = selectArtistsByRecords($_POST['abrid']);
+include "artist-by-records/view-artist-by-records.php";
 include "headfoot/view-footer.php";
 ?>
