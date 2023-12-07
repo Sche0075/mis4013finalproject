d6860b500195deef1a76898633a47fcd2df93f28
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 15:18:00 -0600

    Update 1-artists.php

--- 1-artists.php
+++ 1-artists.php
@@ -1,1 +1,40 @@
+<?php
+require_once("util-db.php");
+require_once("artists/model-artists.php");
 
+$pageTitle = "Artists";
+include "headfoot/view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+     if (insertGenres($_POST['aName'], $_POST['aHometown'])) {
+       echo '<div class="alert alert-success" role="alert">Artist added.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+
+    case "Edit":
+     if (updateGenres($_POST['aName'], $_POST['gHometown'], $_POST['aid'])) {
+       echo '<div class="alert alert-success" role="alert">Artist edited.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+   
+    case "Delete":
+     if (deleteGenres($_POST['aid'])) {
+       echo '<div class="alert alert-success" role="alert">Artist deleted.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+    
+  }  
+}
+
+$artists = selectArtists();
+include "genres/view-genres.php";
+include "headfoot/view-footer.php";
+?>
