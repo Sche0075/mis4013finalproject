818303e85c9ad9e98e87549498a05f2cb4d93012
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 13:52:16 -0600

    Update model-artists-by-records.php

--- artists-by-records/model-artists-by-records.php
+++ artists-by-records/model-artists-by-records.php
@@ -1,6 +1,6 @@
 <?php
 
-function selectArtistByRecords($abrid) {
+function selectArtistsByRecords($abrid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT a.artist_id, a.artist_name, a.artist_hometown, r.record_name, g.genre_name FROM record r JOIN artist a ON a.record_id = r.record_id JOIN genre g ON g.genre_id = a.genre_id Where r.record_id = ?");
