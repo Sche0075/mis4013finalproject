ccd70596ef7c42b6849f17d0fdc0cd88704d8420
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-07 14:09:02 -0600

    Update 5-artists-by-records.php

--- 5-artists-by-records.php
+++ 5-artists-by-records.php
@@ -3,9 +3,7 @@ require_once("util-db.php");
 require_once("artists-by-records/model-artists-by-records.php");
 
 $pageTitle = "Record Label Artists";
-<head>
-  <h2>To edit these you must be on the artist tab</h2>
-</head>
+To edit these you must be on the artist tab
 include "headfoot/view-header.php";
 
 $artistsbyrecords = selectArtistsByRecords($_POST['abrid']);
