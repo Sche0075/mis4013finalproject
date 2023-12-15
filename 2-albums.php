39987b6432a291fd89095f3f8abe5aef7860a444
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 05:29:36 -0600

    Update 2-albums.php

--- 2-albums.php
+++ 2-albums.php
@@ -1,1 +1,40 @@
+<?php
+require_once("util-db.php");
+require_once("artists/model-artists.php");
 
+$pageTitle = "Artists";
+include "headfoot/view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+     if (insertArtists($_POST['rid'] ,$_POST['gid'], $_POST['aName'], $_POST['aHometown'])) {
+       echo '<div class="alert alert-success" role="alert">Artist added.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+
+    case "Edit":
+     if (updateArtists($_POST['rid'] ,$_POST['gid'], $_POST['aName'], $_POST['aHometown'], $_POST['aid'])) {
+       echo '<div class="alert alert-success" role="alert">Artist edited.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+   
+    case "Delete":
+     if (deleteArtists($_POST['aid'])) {
+       echo '<div class="alert alert-success" role="alert">Artist deleted.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+    
+  }  
+}
+
+$artists = selectArtists();
+include "artists/view-artists.php";
+include "headfoot/view-footer.php";
+?>
