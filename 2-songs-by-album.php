6a6986ba83c5b2b7683524d6c9a178f1a3b27e03
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 06:56:12 -0600

    Update 2-songs-by-album.php

--- 2-songs-by-album.php
+++ 2-songs-by-album.php
@@ -1,1 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("songs-by-albums/model-songs-by-albums.php");
 
+$pageTitle = "Songs in Album";
+include "headfoot/view-header.php";
+
+$songsbyalbums = selectSongsByAlbums($_POST['sbalid']);
+include "songs-by-albums/view-songs-by-albums.php";
+include "headfoot/view-footer.php";
+?>
