b96189e9d3cf7ec5cec9b2eabbc17b7050a9e72c
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 06:17:26 -0600

    Update 2-albums.php

--- 2-albums.php
+++ 2-albums.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("albums/model-albums.php");
 
-$pageTitle = "Artists";
+$pageTitle = "Albums";
 include "headfoot/view-header.php";
 
 if (isset($_POST['actionType'])) {
