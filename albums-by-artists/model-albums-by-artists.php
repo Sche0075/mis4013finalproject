2d2b164e4e07d814f374171cc1bc5b8f6ddc2850
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 05:22:19 -0600

    Update model-albums-by-artists.php

--- albums-by-artists/model-albums-by-artists.php
+++ albums-by-artists/model-albums-by-artists.php
@@ -3,7 +3,7 @@
 function selectAlbumsByArtists($abaid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT al.album_id, al.artist_name, al.genre_name, al.album_name, al.album_year FROM artist a JOIN album al ON al.artist_id = a.artist_id JOIN genre g ON g.genre_id = al.genre_id WHERE a.artist_id = ?");
+        $stmt = $conn->prepare("SELECT al.album_id, a.artist_name, g.genre_name, al.album_name, al.album_year FROM artist a JOIN album al ON al.artist_id = a.artist_id JOIN genre g ON g.genre_id = al.genre_id WHERE a.artist_id = ?");
         $stmt->bind_param("i",$abaid);
         $stmt->execute();
         $result = $stmt->get_result();
