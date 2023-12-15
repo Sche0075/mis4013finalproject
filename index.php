76cdd53b2a52c825fef11f6f0e57c8e86c2c55d2
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 04:12:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,14 @@ $pageTitle = "Home";
 include "headfoot/view-header.php";
 ?>
 
+<div style="width:300px;margin-left:auto;margin-right:auto;">
+        <h1>Song Data Base</h1>
+</div>
+
+
 <head>
 <style>
-body {
+body {  
   background-image: url(imageex.jpg);
   background-repeat: no-repeat;
   background-attachment: fixed;  
