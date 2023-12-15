a2691987bfc8404b79d40efd36502cb1628a1eab
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 07:13:38 -0600

    Update model-songs-by-albums.php

--- songs-by-albums/model-songs-by-albums.php
+++ songs-by-albums/model-songs-by-albums.php
@@ -4,7 +4,7 @@ function selectSongsByAlbums($sbalid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT s.song_id, s.song_name, s.song_length, al.album_name FROM album al JOIN song s ON s.album_id = al.album_id WHERE al.album_id = ?");
-        $stmt->bind_param("i",$abalid);
+        $stmt->bind_param("i",$sbalid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
