db0a4bd29a5432958293b09f395778edeb715033
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 05:23:36 -0600

    Update view-artists-by-records.php

--- artists-by-records/view-artists-by-records.php
+++ artists-by-records/view-artists-by-records.php
@@ -34,3 +34,15 @@
       </tbody>
   </table>
 </div>
+
+<head>
+<style>
+body {  
+  background-image: url(imageex.jpg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+</head>
+<body>
