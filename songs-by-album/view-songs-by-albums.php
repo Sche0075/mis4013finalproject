4d1ecbb0aecfdf22ca020d547ba0766fd039c135
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 07:08:35 -0600

    Create view-songs-by-albums.php

---
+++ songs-by-album/view-songs-by-albums.php
@@ -0,0 +1,47 @@
+<h1>Artist's Albums</h1>
+<h5>(To edit these you must return to the Albums tab)</h5>
+<div class="table-responsive">
+  <table class="table">
+    
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Album Name:</th>
+        <th>Genre:</th>
+        <th>Album Name:</th>
+        <th>Album Year:</th>
+      </tr>
+    </thead>
+      
+      <tbody>
+        <?php
+          while ($abaid = $albumsbyartists->fetch_assoc()) {
+        ?>
+            <tr>
+
+                <td><?php echo $abaid['album_id']; ?></td>
+                <td><?php echo $abaid['artist_name']; ?></td> 
+                <td><?php echo $abaid['genre_name']; ?></td>
+                <td><?php echo $abaid['album_name']; ?></td>
+                <td><?php echo $abaid['album_year']; ?></td>
+                              
+            </tr>
+
+        <?php
+          }
+        ?>          
+      </tbody>
+  </table>
+</div>
+
+<head>
+<style>
+body {  
+  background-image: url(imageex.jpg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+</head>
+<body>
