40f7508c240c8a1e322fafb39b39712ff913e4c4
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 09:12:36 -0600

    Update view-songs-charts.php

--- charts/view-songs-charts.php
+++ charts/view-songs-charts.php
@@ -1,15 +1,1 @@
-<h1>Genre Songs Chart</h1>
-<div class="table-responsive">
-<table class="table">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Offic</th>      
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-while ($
-  </tbody>
-</div>
+
