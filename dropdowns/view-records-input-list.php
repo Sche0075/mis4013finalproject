d841f5080885be8970b894da52034d83e264e021
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 02:43:19 -0600

    Update view-records-input-list.php

--- dropdowns/view-records-input-list.php
+++ dropdowns/view-records-input-list.php
@@ -1,6 +1,8 @@
 <select class="form-select" id="rid" name="rid">
 <?php
 while ($recordsItem = $recordsList->fetch_assoc()) {
+  $selText = "";
+  if($selectedRecord
 ?>
   <option value="<?php echo $recordsItem['record_id']; ?>"><?php echo $recordsItem['record_name']; ?></option>
 <?php
