22b5b0bddc8ff52b4f47a3fe71b2ae2729ce7344
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 09:14:20 -0600

    Update songs-chart.php

--- songs-chart.php
+++ songs-chart.php
@@ -1,11 +1,1 @@
-<?php
-require_once("util-db.php");
-require_once("charts/model-songs-charts.php");
 
-$pageTitle = "Songs Chart by Genre";
-include "headfoot/view-header.php"
-
-$songs = selectSongs();
-include "charts/view-songs-charts.php";
-include "headfoot/view-footer.php";
-?>
