37a168f73764f0f9cb405868ebc5a6a8038482e4
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 04:15:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ include "headfoot/view-header.php";
 
 <div style="width:300px;margin-left:auto;margin-right:auto;margin-top:50px;">
         <h1>Song Data Base</h1>
+        </br>
+        <h3>By Huston Bryan and Jameson Scheetz</h3>
 </div>
 
 
