28761a14ab2bb1e16ee7567c660e8c4cd60cec94
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-14 20:48:49 -0600

    Update view-header.php

--- headfoot/view-header.php
+++ headfoot/view-header.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
+    <div classs="container">
        <nav class="navbar navbar-expand-lg bg-primary text-white">
           <div class="container-fluid">
              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-music-note-list" viewBox="0 0 16 16">
