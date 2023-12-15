39957b6c9812a74f7ae2ab449112f438da1ec903
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-14 20:46:08 -0600

    Update view-header.php

--- headfoot/view-header.php
+++ headfoot/view-header.php
@@ -8,7 +8,7 @@
   </head>
   <body>
     <div classs="container">
-       <nav class="navbar navbar-expand-lg bg-primary">
+       <nav class="navbar navbar-expand-lg bg-primary text-white">
           <div class="container-fluid">
              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-music-note-list" viewBox="0 0 16 16">
                 <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2"/>
