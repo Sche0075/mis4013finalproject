db53b04d1179c23ae6296d90b2fce235938fc0ac
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 05:04:20 -0600

    Update albums-by-artists.php

--- albums-by-artist/albums-by-artists.php
+++ albums-by-artist/albums-by-artists.php
@@ -3,7 +3,7 @@
 function selectAlbumssByArtists($abaid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT al.album_id, al.artist_id, al.genre_id, al.album_name, al.album_year FROM artist a JOIN album al ON al.artist_id = a.artist_id JOIN genre g ON g.genre_id = al.genre_id WHERE a.artist_id = ?");
+        $stmt = $conn->prepare("SELECT al.album_id, al.artist_name, al.genre_name, al.album_name, al.album_year FROM artist a JOIN album al ON al.artist_id = a.artist_id JOIN genre g ON g.genre_id = al.genre_id WHERE a.artist_id = ?");
         $stmt->bind_param("i",$abaid);
         $stmt->execute();
         $result = $stmt->get_result();
