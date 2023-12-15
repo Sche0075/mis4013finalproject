d2cd432866d70ba5c754b1de64f291bb642a4a6e
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 07:48:06 -0600

    Update model-songs.php

--- songs/model-songs.php
+++ songs/model-songs.php
@@ -3,7 +3,7 @@
 function selectSongs() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT song_id, song_name, song_length FROM `song`");
+        $stmt = $conn->prepare("SELECT s.song_id, al.album_name, a.artist_name, g.genre_name, s.song_name, s.song_length FROM genre g JOIN artist a On a.genre_id = g.genre_id JOIN album al ON al.artist_id = a.artist_id JOIN song s ON s.album_id = al.album_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -14,11 +14,53 @@ function selectSongs() {
     }
 }
 
-function insertSongs($sName, $sLength) {
+function selectArtistsforInput() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `song` (`song_name`, `song_length`) VALUES (?, ?)");
-        $stmt->bind_param("ss", $sName, $sLength);
+        $stmt = $conn->prepare("SELECT artist_id, artist_name FROM `artist` order by artist_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectAlbumsforInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT album_id, album_name FROM `album` order by album_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectGenresforInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT genre_id, genre_name FROM `genre` order by genre_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertSongs($aid, $alid, $gid, $sName, $sLength) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `song` (`artist_id`, `album_id`, `genre_id`, `song_name`, `song_length`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("iiiss", $aid, $alid, $gid, $alName, $alYear);
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -28,11 +70,11 @@ function insertSongs($sName, $sLength) {
     }
 }
 
-function updateSongs($sName, $sLength, $sid) {
+function updateSongs($aid, $alid, $gid, $sName, $sLength, $sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("update `song` set `song_name` = ?, `song_length` = ? where song_id = ?");
-        $stmt->bind_param("ssi", $sName, $sLength, $sid);
+        $stmt = $conn->prepare("update `song` set `artist_id` = ?, `album_id` = ?, `genre_id` = ?, `song_name` = ?, `song_length` = ? where song_id = ?");
+        $stmt->bind_param("iiissi", $aid, $alid, $gid, $sName, $sLength, $sid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
