0a66c3dd726d0638a142035fae4817b20c353736
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 03:52:20 -0600

    Update view-genres-input-list.php

--- dropdowns/view-genres-input-list.php
+++ dropdowns/view-genres-input-list.php
@@ -2,7 +2,7 @@
 <?php
 while ($genresItem = $genresList->fetch_assoc()) {
   $selText = "";
-  if($selectedGenre == $genresItem['genre_id']) 
+  if($selectedRecord == $genresItem['genre_id']) {
     $selText = " selected";
   }
 ?>
