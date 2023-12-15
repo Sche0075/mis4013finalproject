e1720df059b66e5ee000e10b72e0ae6bcc9c1887
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 02:02:20 -0600

    Update view-records-input-list.php

--- dropdowns/view-records-input-list.php
+++ dropdowns/view-records-input-list.php
@@ -2,7 +2,7 @@
 <?php
 while ($recordsItem = $recordsList->fetch_assoc()) {
 ?>
-  <option value="<?php echo $recordsItem[`record_id`]; ?>"><?php echo $recordsItem[`record_name`]; ?></option>
+  <option value="<?php echo $recordsItem['record_id']; ?>"><?php echo $recordsItem['record_name']; ?></option>
 <?php
 }
 ?>
