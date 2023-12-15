ecd41d2b19225f04e78a2e09ab8f347ca53694ab
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 04:43:55 -0600

    Update 1-albums-by-artists.php

--- 1-albums-by-artists.php
+++ 1-albums-by-artists.php
@@ -1,1 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("albums-by-artists/model-albums-by-artists.php");
 
+$pageTitle = "Artist's Albums";
+include "headfoot/view-header.php";
+
+$albumsbyartists = selectAlbumsByArtists($_POST['abaid']);
+include "albums-by-artists/view-albums-by-artists.php";
+include "headfoot/view-footer.php";
+?>
