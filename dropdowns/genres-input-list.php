cf7da4a2dd38f41d1f9ea414909d5d182dff1dd9
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 02:12:23 -0600

    Create genres-input-list.php

---
+++ dropdowns/genres-input-list.php
@@ -0,0 +1,9 @@
+<select class="form-select" id="gid" name="gid">
+<?php
+while ($genresItem = $genresList->fetch_assoc()) {
+?>
+  <option value="<?php echo $genresItem['genre_id']; ?>"><?php echo $genresItem['genre_name']; ?></option>
+<?php
+}
+?>
+</select>
