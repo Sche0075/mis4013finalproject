24d20b554e80f8b4002c80bc3a7d4cc1b0630685
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 04:03:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,17 @@ $pageTitle = "Home";
 include "headfoot/view-header.php";
 ?>
 
-<div style="width:300px;margin-left:auto;margin-right:auto;">
-        <h1>Song Data Base</h1>
-</div>
+<head>
+<style>
+body {
+  background-image: url('img_girl.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+</head>
+<body>
       
 <?php
 include "headfoot/view-footer.php";
