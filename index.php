724b55fab70bb65a2be3dfe8e2095b2bfbd425a1
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 04:14:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "headfoot/view-header.php";
 ?>
 
-<div style="width:300px;margin-left:auto;margin-right:auto;margin-top:15px;">
+<div style="width:300px;margin-left:auto;margin-right:auto;margin-top:50px;">
         <h1>Song Data Base</h1>
 </div>
 
