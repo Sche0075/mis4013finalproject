d29d7b919f518d7f935737270c7e425ca7f718be
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 08:38:54 -0600

    Update view-albums-input-list.php

--- dropdowns/view-albums-input-list.php
+++ dropdowns/view-albums-input-list.php
@@ -1,13 +1,1 @@
-<select class="form-select" id="alid" name="alid">
-<?php
-while ($albumsItem = $albumsList->fetch_assoc()) {
-  $selText = "";
-  if($selectedAlbum == $albumsItem['album_id']) {
-    $selText = " selected";
-  }
-?>
-  <option value="<?php echo $albumsItem['album_id']; ?>"<?=$selText?>><?php echo $albumsItem['album_name']; ?></option>
-<?php
-}
-?>
-</select>
+
