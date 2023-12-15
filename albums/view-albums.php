5c2fa8e114c11583f13ba211d1bbd07ae4e52334
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 07:15:57 -0600

    Update view-albums.php

--- albums/view-albums.php
+++ albums/view-albums.php
@@ -52,7 +52,7 @@
               
                 <td>
                       <form method="post" action="2-songs-by-albums.php">
-                        <input type="hidden" name="alid" value="<?php echo $album['album_id']; ?>">
+                        <input type="hidden" name="sbalid" value="<?php echo $album['album_id']; ?>">
                         <button type="submit" class="btn btn-primary">Songs</button>
                       </form>
                 </td>
