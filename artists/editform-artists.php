26e17b1eb0f9422e2046eceab6a4ddc3b384a056
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 00:37:33 -0600

    Update editform-artists.php

--- artists/editform-artists.php
+++ artists/editform-artists.php
@@ -18,12 +18,12 @@
                                 
                                     <div class="mb-3">
                                       <label for="rid<?php echo $artist['artist_id']; ?>" class="form-label">Record Label:</label>
-                                      <input type="text" class="form-control" id="rid<?php echo $artist['artist_id']; ?>" name="rid" value="<?php echo $artist['artist_name']; ?>">
+                                      <input type="text" class="form-control" id="rid<?php echo $artist['artist_id']; ?>" name="rid" value="<?php echo $artist['rid']; ?>">
                                     </div>
                                 
                                     <div class="mb-3">
                                       <label for="gid<?php echo $artist['artist_id']; ?>" class="form-label">Genre:</label>
-                                      <input type="text" class="form-control" id="gid<?php echo $artist['artist_id']; ?>" name="gid" value="<?php echo $artist['artist_name']; ?>">
+                                      <input type="text" class="form-control" id="gid<?php echo $artist['artist_id']; ?>" name="gid" value="<?php echo $artist['gid']; ?>">
                                     </div>                                    
                                 
                                     <div class="mb-3">
