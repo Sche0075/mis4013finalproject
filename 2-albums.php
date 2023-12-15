cbf22b1fdf1c5d2bd74cd6373b9d91a277324279
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-14 20:31:55 -0600

    Update 2-albums.php

--- 2-albums.php
+++ 2-albums.php
@@ -1,1 +1,40 @@
+<?php
+require_once("util-db.php");
+require_once("albums/model-albums.php");
 
+$pageTitle = "Albums";
+include "headfoot/view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+     if (insertAlbums($_POST['alName'], $_POST['alYear'])) {
+       echo '<div class="alert alert-success" role="alert">Album added.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+
+    case "Edit":
+     if (updateAlbums($_POST['alName'], $_POST['alYear'], $_POST['alid'])) {
+       echo '<div class="alert alert-success" role="alert">Album edited.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+   
+    case "Delete":
+     if (deleteAlbums($_POST['alid'])) {
+       echo '<div class="alert alert-success" role="alert">Album deleted.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
+    
+  }  
+}
+
+$artists = selectArtists();
+include "albumss/view-albumss.php";
+include "headfoot/view-footer.php";
+?>
