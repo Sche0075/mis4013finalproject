0d845ed61aaf9c64c5271a836e0ee2bbd75551ad
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 07:07:41 -0600

    Update model-songs-by-albums.php

--- songs-by-album/model-songs-by-albums.php
+++ songs-by-album/model-songs-by-albums.php
@@ -1,1 +1,18 @@
+<?php
 
+function selectSongsByAlbums($sbalid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT s.song_id, s.song_name, s.song_length, al.album_name FROM album al JOIN song s ON s.album_id = al.album_id WHERE al.album_id = ?");
+        $stmt->bind_param("i",$abalid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
