f9d712399824cc5486a8070708d76f219d6083bd
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 18:15:32 -0600

    Update view-charts.php

--- charts/view-charts.php
+++ charts/view-charts.php
@@ -1,17 +1,8 @@
 <h1>Count of Songs in Genres</h1>
-<div class="table-responsive">
-<table class="table">
-<thead>
-  <tr>
-    <th></th>
-  </tr>
-</thead>
-<tbody>
+
   <?php
 while ($genre = $genres->fetch_assoc()) {
-  include "model-charts.php"
+  
   }
     ?>
-</tbody>
-</table>
-</div>
+
