5ad1e807fdc14c0d579b539ffcd2778f26504ae5
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 18:53:10 -0600

    Update charts.php

--- charts.php
+++ charts.php
@@ -5,7 +5,7 @@ require_once("charts/model-charts.php");
 $pageTitle = "Charts";
 include "headfoot/view-header.php";
 
-$genres = selectchartGenres();
+$genres = selectGenres();
 include "charts/view-charts.php";
 include "headfoot/view-footer.php";
 ?>
