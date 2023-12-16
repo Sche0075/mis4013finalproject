36e273310f6d58ee372cd27034049e21c9664854
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 20:01:24 -0600

    Create models-charts2.php

---
+++ charts/models-charts2.php
@@ -0,0 +1,15 @@
+<?php
+function selectchartGenres() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT genre_name, count(s.song_id) as count_song FROM `genre` g join song s on s.genre_id=g.genre_id group by genre_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+};
+?>
