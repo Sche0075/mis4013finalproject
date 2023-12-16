527714a995f3ce2e56343cbdba9f59e631f5880e
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 19:08:19 -0600

    Update model-charts.php

--- charts/model-charts.php
+++ charts/model-charts.php
@@ -1,8 +1,8 @@
 <?php
-function selectGenres() {
+function selectchartGenres() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT genre_name, count(s.song_id) as count_song FROM genre join song s on s.genre_id=g.genre_id group by genre_name");
+        $stmt = $conn->prepare("SELECT genre_name, count(s.song_id) as count_song FROM `genre` join song s on s.genre_id=g.genre_id group by genre_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
