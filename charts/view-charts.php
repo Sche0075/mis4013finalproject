298067dc2f63d35646f43a7a312410ba3ec6e635
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 18:29:27 -0600

    Update view-charts.php

--- charts/view-charts.php
+++ charts/view-charts.php
@@ -1,8 +1,44 @@
 <h1>Count of Songs in Genres</h1>
 
-  <?php
-while ($genre = $genres->fetch_assoc()) {
-  
-  }
-  ?>
+<div>
+  <canvas id="Chart1"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [
+        <?php
+            while ($genre = $genres->fetch_assoc()) {
+              echo $genre['genre_name'] . ", ";
+            }
+            ?>],
+      datasets: [{
+        label: '# of Songs',
+        data: [
+          <?php
+            $genres = selectGenres
+            while ($genre = $genres->fetch_assoc()) {
+              echo $genre['count_songs'] . ", ";
+            }
+            ?>
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
 
