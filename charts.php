166bf85629d193d8db7f9fa9866d346c9c6dd912
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 18:07:13 -0600

    Update charts.php

--- charts.php
+++ charts.php
@@ -1,1 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("charts/model-charts.php");
 
+$pageTitle = "Charts";
+include "headfoot/view-header.php";
+
+$genres = selectGenres();
+include "charts/view-charts.php";
+include "headfoot/view-footer.php";
+?>
