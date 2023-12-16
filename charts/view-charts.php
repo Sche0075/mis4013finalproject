d0381f07767d8e87932e360f3a447acd2dc996a1
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-12-15 20:10:47 -0600

    Update view-charts.php

--- charts/view-charts.php
+++ charts/view-charts.php
@@ -63,3 +63,15 @@
     }
   });
 </script>
+
+<head>
+<style>
+body {  
+  background-image: url(imageex.jpg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+</head>
+<body>
